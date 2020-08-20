36ccf6b3ccef8232a0f0f625f9a7972a068b2598
junjun <zhangjunjun1989@163.com>
2020-08-20 23:05:45 +0800

    feat: 初步添加swagger

--- src/main/java/com/iflyteck/controller/UserController.java
+++ src/main/java/com/iflyteck/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.iflyteck.controller;
 
+import com.iflyteck.domain.Account;
 import com.iflyteck.domain.User;
+import com.iflyteck.repository.AccountRespository;
 import com.iflyteck.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,14 +12,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@RequestMapping("/test")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AccountRespository accountRespository;
 
     @RequestMapping("/testUser")
     @ResponseBody
     public List<User> testUser () {
         return userService.queryUserList();
     }
+
+    @RequestMapping("/testAccount")
+    @ResponseBody
+    public List<Account> testAccount () {
+        return accountRespository.findAll();
+    }
+
 }
