5d5eb0071a378c24c6be5474a9651af9039bc1a1
junjun <zhangjunjun1989@163.com>
2020-08-21 12:57:30 +0800

    feat: SpringBoot整合Swagger代码实现

--- src/main/java/com/iflyteck/config/SwaggerConfiguration.java
+++ src/main/java/com/iflyteck/config/SwaggerConfiguration.java
@@ -1,7 +1,35 @@
 package com.iflyteck.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
+
+    @Bean
+    public Docket createRestApi () {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Controller.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo () {
+        return new ApiInfoBuilder()
+                .title("Reader-Project")
+                .description("Description")
+                .version("1.0.0")
+                .build();
+    }
 }
--- src/main/java/com/iflyteck/controller/UserController.java
+++ src/main/java/com/iflyteck/controller/UserController.java
@@ -4,15 +4,19 @@ import com.iflyteck.domain.Account;
 import com.iflyteck.domain.User;
 import com.iflyteck.repository.AccountRespository;
 import com.iflyteck.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/test")
+@Api(tags = "springboot项目测试接口")
 public class UserController {
 
     @Autowired
@@ -21,14 +25,16 @@ public class UserController {
     @Autowired
     private AccountRespository accountRespository;
 
-    @RequestMapping("/testUser")
+    @RequestMapping(value = "/testUser", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "查询所有用户", notes = "测试试验使用")
     public List<User> testUser () {
         return userService.queryUserList();
     }
 
-    @RequestMapping("/testAccount")
+    @RequestMapping(value = "/testAccount",  method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "查询所有账户", notes = "测试查询使用")
     public List<Account> testAccount () {
         return accountRespository.findAll();
     }
